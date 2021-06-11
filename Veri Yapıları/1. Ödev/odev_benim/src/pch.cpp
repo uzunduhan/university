8b62c3638e5ee071d438ffa80a33fd4f2743e24d
Duhan Uzun <51044577+uzunduhan@users.noreply.github.com>
2021-06-11 10:31:16 +0300

    Delete Veri Yapıları/1. Ödev/odev_benim directory

--- Veri Yapıları/1. Ödev/odev_benim/src/pch.cpp
+++
@@ -1,5 +0,0 @@
-﻿// pch.cpp: Önceden derlenen üst bilgiye karşılık gelen kaynak dosya, tamamlamanın başarılı olması için gerekli
-
-#include "pch.h"
-
-// Bu dosyayı genel anlamda yoksayın, ancak önceden derlenmiş üst bilgiler kullanıyorsanız kolayca erişebileceğiniz bir yerde tutun.
